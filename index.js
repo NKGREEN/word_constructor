f9596f6bb210d1555f95279435a57265d3df3b1c
NKGREEN <green.nadiak@gmail.com>
2018-09-02 01:34:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Contains logic of the game, depends on Word.js
 
+const Word = require("./word.js");
 
+//Introduces and explains game to user
 function init() {
   pickedWords = [];
   console.log("Nothing Compares 2 Prince: Song Title Guessing Game");
@@ -8,6 +10,7 @@ function init() {
   console.log("Guess the song title from a)song's first line and b)guessing letters");
   console.log("You have eight chances to guess the correct letters.");
   console.log("Let's play!");
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
   playGame();
 }
 //Randomly selects a word and uses the Word constructor to store it
