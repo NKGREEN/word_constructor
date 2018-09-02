3637fd9186b3a0b5c3a5c29cf601e26e4469d56e
NKGREEN <green.nadiak@gmail.com>
2018-09-01 23:01:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+//Contains logic of the game, depends on Word.js
 
+
+//Randomly selects a word and uses the Word constructor to store it
+
+//Prompts the user for guesses and kees track of user's remaining guesses
